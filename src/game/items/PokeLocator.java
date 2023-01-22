da6df081c4e073456315a93f538e5745526aa184
ZecanLiu <zliu0207@student.monash.edu>
2023-01-22 22:40:08 +1100

    Fixed bugs

--- src/game/items/PokeLocator.java
+++ src/game/items/PokeLocator.java
@@ -1,7 +1,7 @@
 package game.items;
 
 import edu.monash.fit2099.engine.items.Item;
-import game.action.IntantTransmissionAction;
+import game.action.InstantTransmissionAction;
 import game.pokemon.Mudkip;
 import game.pokemon.Torchic;
 import game.pokemon.Treecko;
@@ -17,9 +17,9 @@ public  class PokeLocator extends Item {
 
     public PokeLocator() {
         super("PokeLocator", '!' ,false);
-        this.addAction(new IntantTransmissionAction(new Mudkip()));
-        this.addAction(new IntantTransmissionAction(new Torchic()));
-        this.addAction(new IntantTransmissionAction(new Treecko()));
+        this.addAction(new InstantTransmissionAction(new Mudkip()));
+        this.addAction(new InstantTransmissionAction(new Torchic()));
+        this.addAction(new InstantTransmissionAction(new Treecko()));
 
     }
 
